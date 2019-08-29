c7b186bd1a82743e618004966dab745de74f0739
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-29 20:46:21 +0200

    added choices for tags and authors

--- quotes_scraping_using_selenium/app.py
+++ quotes_scraping_using_selenium/app.py
@@ -6,8 +6,19 @@ chrome.get('http://quotes.toscrape.com/search.aspx')
 
 page = QuotesPage(chrome)
 
+authors = page.get_available_authors()
+print(f'Choose one of these authors [{" | ".join(authors)}]')
+
 author = input("Enter author name: ")
 page.select_author(author)
 
+tags = page.get_available_tags()  # returns a list of all tags available for specific author
+print(f'Choose one of these tags: [{" | ".join(tags)}]')
+
+
 tag = input("Enter tag name: ")
 page.select_tag(tag)
+
+page.search_button.click()
+
+print(page.quotes)
